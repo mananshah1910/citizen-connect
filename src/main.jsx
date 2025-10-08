340024f29dd3970faf1a1415cbea1b2236403bbe
mananshah1910 <your_email@example.com>
2025-10-08 21:38:03 +0530

    Project initialized

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/homepage/Homepage.jsx'
+import AddService from './pages/addservice/addService.jsx'
+import AdminLogin from './pages/login/Login.jsx'
+import AllService from './pages/service/service.jsx'
+
+
+
+createRoot(document.getElementById('root')).render(
+
+  <BrowserRouter basename="">
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/addservice" element={<AddService/>}></Route>
+      <Route path="/login" element={<AdminLogin/>}></Route>
+      <Route path="/service" element={<AllService/>}></Route>
+    </Routes>
+  </BrowserRouter>
+)
